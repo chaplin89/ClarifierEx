7b7bad7fe2cda21894a634ba745889f74d4f55c1
Marco <chaplin89@gmail.com>
2016-05-23 19:20:27 +0200

    Changes to ConfuserProjectsGenerator: -Added the possibility to get arguments from the command line -Reorganized code

--- Clarifier.Test/TestGenerator/Program.cs
+++ Clarifier.Test/TestGenerator/Program.cs
@@ -7,6 +7,7 @@ using Confuser.Core.Project;
 using System.Xml;
 using Confuser.Core;
 using System.IO;
+using System.Diagnostics;
 
 namespace Clarifier.Test.TestGenerator
 {
@@ -39,21 +40,24 @@ namespace Clarifier.Test.TestGenerator
 
     class Program
     {
+        static private string outputPath = Directory.GetCurrentDirectory() + @"\Obfuscated\";
+        static private string inputPath = Directory.GetCurrentDirectory() + @"\Unobfuscated\";
+        static private string outputProject = Directory.GetCurrentDirectory() + @"\OutputConfuserProject.crproj";
+        static private bool invokeObfuscator = true;
+        static private string confuserPath = Directory.GetCurrentDirectory() + @"\..\..\..\ConfuserEx\Debug\Bin\Confuser.CLI.exe";
+
         static List<ModuleDescriptor> registeredApplications = new List<ModuleDescriptor>()
         {
-            new ModuleDescriptor("SimpleTestApplication.exe")
+            new ModuleDescriptor("SimpleConsoleApplication.exe")
             {
                 protections = new List<ProtectionType>
                 {
                     ProtectionType.AntiDebug,
                     ProtectionType.AntiTamper,
-                    ProtectionType.Constants
+                    ProtectionType.Constants,
                 }
             }
         };
-
-        static private readonly string outputPath = @"..\Obfuscated\";
-        static private readonly string inputPath = @".\";
 
         static private readonly Dictionary<ProtectionType, string> mapProtectionType = new Dictionary<ProtectionType, string>()
         {
@@ -71,14 +75,36 @@ namespace Clarifier.Test.TestGenerator
 
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+                inputPath = args[0];
+            if (args.Length > 1)
+                outputPath = args[1];
+            if (args.Length > 2)
+                outputProject = args[2];
+            if (args.Length > 4)
+            {
+                bool.TryParse(args[3], out invokeObfuscator);
+                confuserPath = args[4] + "Confuser.CLI.exe";
+            }
+
+            Directory.CreateDirectory(inputPath);
+            Directory.CreateDirectory(outputPath);
+            Directory.GetParent(outputProject).Create();
+
+            Debug.Assert(Directory.Exists(inputPath));
+            Debug.Assert(Directory.Exists(outputPath));
+            Debug.Assert(Directory.GetParent(outputProject).Exists);
+            Debug.Assert(!invokeObfuscator  || File.Exists(confuserPath));
+
             ConfuserProject module = new ConfuserProject()
             {
-                OutputDirectory = Directory.GetCurrentDirectory() + outputPath,
-                BaseDirectory = Directory.GetCurrentDirectory() + inputPath
+                OutputDirectory = outputPath,
+                BaseDirectory = inputPath
             };
 
             foreach (var v in registeredApplications)
             {
+                Console.WriteLine("Processing {0}", v.inputFileName);
                 ProjectModule project = new ProjectModule() { Path = v.inputFileName };
                 Rule moduleRule = new Rule();
 
@@ -87,16 +113,26 @@ namespace Clarifier.Test.TestGenerator
                     string protection = mapProtectionType[vv];
                     SettingItem<Protection> currentProtection = new SettingItem<Protection>(protection);
                     moduleRule.Add(currentProtection);
+                    Console.WriteLine("\tAdded protection: {0}", protection);
                 }
 
                 project.Rules.Add(moduleRule);
                 module.Add(project);
             }
 
+            Console.WriteLine("Saving project: {0}", outputProject);
             XmlDocument document = module.Save();
 
-            using (XmlWriter xmlWrite = XmlWriter.Create(@".\OutputConfuserProject.crproj"))
+            using (XmlWriter xmlWrite = XmlWriter.Create(outputProject))
                 document.WriteContentTo(xmlWrite);
+
+            if (invokeObfuscator)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Invoke obfuscator required. Invoking.");
+                Process.Start(confuserPath, outputProject).WaitForExit();
+                Console.WriteLine("Done. Exiting.");
+            }
         }
     }
 }
