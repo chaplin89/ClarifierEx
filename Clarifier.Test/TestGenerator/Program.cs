9231260b3c2a5db5015e74b86d2ef629d746032e
marco.fornaro <chaplin89@gmail.com>
2016-05-23 17:05:54 +0200

    First working version of TestGenerator

--- Clarifier.Test/TestGenerator/Program.cs
+++ Clarifier.Test/TestGenerator/Program.cs
@@ -5,45 +5,98 @@ using System.Text;
 using System.Threading.Tasks;
 using Confuser.Core.Project;
 using System.Xml;
+using Confuser.Core;
+using System.IO;
 
 namespace Clarifier.Test.TestGenerator
 {
-    class ModuleDescriptor
+    internal class ModuleDescriptor
     {
+        public string inputFileName;
+        public List<ProtectionType> protections;
+        public string outputFileName;
+        public ModuleDescriptor(string inputFileName)
+        {
+            this.inputFileName = inputFileName;
+            outputFileName = "Obfuscated" + inputFileName;
+            protections = new List<ProtectionType>();
+        }
+    }
 
+    enum ProtectionType
+    {
+        AntiDebug,
+        AntiILDasm,
+        AntiTamper,
+        Constants,
+        ControlFlow,
+        AntiDump,
+        InvalidMetadata,
+        ReferenceProxy,
+        Resources,
+        Rename
     }
 
     class Program
     {
-        static private Dictionary<string,List<string>> registeredApplications = new Dictionary<string, List<string>>
+        static List<ModuleDescriptor> registeredApplications = new List<ModuleDescriptor>()
         {
+            new ModuleDescriptor("SimpleTestApplication.exe")
             {
-                "SimpleConsoleApplication.exe",  new List<string>
+                protections = new List<ProtectionType>
                 {
-                    ""
+                    ProtectionType.AntiDebug,
+                    ProtectionType.AntiTamper,
+                    ProtectionType.Constants
                 }
-            },
+            }
         };
 
+        static private readonly string outputPath = @"..\Obfuscated\";
+        static private readonly string inputPath = @".\";
+
+        static private readonly Dictionary<ProtectionType, string> mapProtectionType = new Dictionary<ProtectionType, string>()
+        {
+            {ProtectionType.AntiDebug, "anti debug" },
+            {ProtectionType.AntiILDasm, "anti ildasm" },
+            {ProtectionType.AntiTamper, "anti tamper" },
+            {ProtectionType.Constants, "constants" },
+            {ProtectionType.ControlFlow, "ctrl flow" },
+            {ProtectionType.AntiDump,"anti dump" },
+            {ProtectionType.InvalidMetadata,"invalid metadata" },
+            {ProtectionType.ReferenceProxy,"ref proxy" },
+            {ProtectionType.Resources,"resources" },
+            {ProtectionType.Rename, "rename" },
+        };
 
         static void Main(string[] args)
         {
-            ConfuserProject module = new ConfuserProject();
+            ConfuserProject module = new ConfuserProject()
+            {
+                OutputDirectory = Directory.GetCurrentDirectory() + outputPath,
+                BaseDirectory = Directory.GetCurrentDirectory() + inputPath
+            };
+
             foreach (var v in registeredApplications)
             {
-                ProjectModule project = new ProjectModule();
-
+                ProjectModule project = new ProjectModule() { Path = v.inputFileName };
                 Rule moduleRule = new Rule();
-                SettingItem<Confuser.Core.Protection> setting = new SettingItem<Confuser.Core.Protection>();
 
-                moduleRule.Add(setting);
+                foreach (var vv in v.protections)
+                {
+                    string protection = mapProtectionType[vv];
+                    SettingItem<Protection> currentProtection = new SettingItem<Protection>(protection);
+                    moduleRule.Add(currentProtection);
+                }
+
                 project.Rules.Add(moduleRule);
                 module.Add(project);
             }
 
             XmlDocument document = module.Save();
-            XmlWriter xmlWrite = XmlWriter.Create(@".\OutputConfuserProject.crproj");
-            document.WriteContentTo(xmlWrite);
+
+            using (XmlWriter xmlWrite = XmlWriter.Create(@".\OutputConfuserProject.crproj"))
+                document.WriteContentTo(xmlWrite);
         }
     }
 }
