040401aa233d2da1e39b156b08226832e815413f
marco.fornaro <chaplin89@gmail.com>
2016-05-23 08:41:19 +0200

    Added the test program and ConfuserEx in the solution

---
+++ Clarifier.Test/SimpleConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an obfuscated program! Deobfuscate me!");
+        }
+    }
+}
