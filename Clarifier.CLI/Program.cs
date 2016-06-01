f1f0d41cb648e8b9b27fa2f209d5bb6f33c8f4a4
marco.fornaro <chaplin89@gmail.com>
2016-06-01 17:07:02 +0200

    Added invokation of TestGenerator as post-build action

--- Clarifier.CLI/Program.cs
+++ Clarifier.CLI/Program.cs
@@ -1,69 +1,47 @@
-﻿using System.Collections.Generic;
-using Clarifier.Core;
-using dnlib.DotNet;
-using System.Linq;
-using System.IO;
-using System.Reflection;
-using System;
+﻿using dnlib.DotNet;
 using System.Diagnostics;
 using Clarifier.Identification.Impl;
-using dnlib.DotNet.Writer;
+using System.IO;
 
 namespace Clarifier.CLI
 {
-
-    class WriteListener : IModuleWriterListener
-    {
-        public void OnWriterEvent(ModuleWriterBase writer, ModuleWriterEvent evt)
-        {
-            switch(evt)
-            {
-                
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Debug.Assert(args.Length > 0);
-            ModuleDefMD targetModule = ModuleDefMD.Load(Directory.GetCurrentDirectory() + args[0]);
-            ModuleDefMD runtimeModule = ModuleDefMD.Load(@".\Confuser.Runtime.dll");
-            NativeModuleWriterOptions options = new NativeModuleWriterOptions(targetModule);
-            ClarifierContext ctx = new ClarifierContext() { CurrentModule = targetModule };
+            ModuleDefMD targetModule = ModuleDefMD.Load(args[0]);
+            ModuleDefMD runtimeModule = ModuleDefMD.Load("Confuser.Runtime.dll");
 
-            Inliner inl = new Inliner();
-
-            inl.PerformIdentification(ctx);
-            inl.PerformRemoval(ctx);
-            targetModule.Write(@".\Unobfuscated.exe");
-            return;
+            ClarifierContext ctx = new ClarifierContext { CurrentModule = targetModule };
 
             AntiDumpIdentification antiDump = new AntiDumpIdentification();
             AntiDebugIdentification antiDebug = new AntiDebugIdentification();
             Constants constants = new Constants();
             AntiTamper antiTamper = new AntiTamper();
+            Inliner inliner = new Inliner();
 
+            inliner.PerformIdentification(ctx);
+            inliner.PerformRemoval(ctx);
 
-            antiTamper.Initialize();
-            antiTamper.PerformIdentification(ctx);
-            antiTamper.PerformRemoval(ctx);
-            //NativeModuleWriterOptions options = new NativeModuleWriterOptions(targetModule);
+//             antiTamper.Initialize();
+//             antiTamper.PerformIdentification(ctx);
+//             antiTamper.PerformRemoval(ctx);
+// 
+//             antiDump.Initialize(ctx);
+//             antiDump.PerformIdentification(ctx);
+//             antiDump.PerformRemoval(ctx);
+// 
+//             antiDebug.Initialize(ctx);
+//             antiDebug.PerformIdentification(ctx);
+//             antiDebug.PerformRemoval(ctx);
+// 
+//             constants.Initialize(ctx);
+//             constants.PerformIdentification(ctx);
+//             constants.PerformRemoval(ctx);
 
-            antiDump.Initialize(ctx);
-            antiDump.PerformIdentification(ctx);
-            antiDump.PerformRemoval(ctx);
-
-            antiDebug.Initialize(ctx);
-            antiDebug.PerformIdentification(ctx);
-            antiDebug.PerformRemoval(ctx);
-
-            constants.Initialize(ctx);
-            constants.PerformIdentification(ctx);
-            constants.PerformRemoval(ctx);
-
-            //File.Delete(@"..\Obfuscated\Unobfuscated.exe");
+            targetModule.Write(@".\Test\Obfuscated\Result.exe");
+            return;
         }
     }
 }
