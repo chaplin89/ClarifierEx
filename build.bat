29cd7516b090fc09a51e721d13236d976b3c9ed5
marco.fornaro <chaplin89@gmail.com>
2016-06-01 16:10:33 +0200

    Fixed some references; Added a build script; Added pre-build action in order to copy assemblies from ConfuserEx

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+call "%VS140COMNTOOLS%\VsDevCmd.bat"
+msbuild .\ConfuserEx\Confuser2.sln /t:Build /p:Configuration=Debug
+msbuild .\ConfuserEx\Confuser2.sln /t:Build /p:Configuration=Release
